6df22516d93bcad12bf10a59fec9ba50085b73a9
kartik <kartikprakash3775@gmail.com>
2019-08-16 19:01:48 -0500

    completed lse

--- least_squares_estimation.py
+++ least_squares_estimation.py
@@ -1,8 +1,19 @@
-import numpy
+########
+# BASIC CODE FOR ESTIMATION OF MEASUREMENTS USING LEAST SQUARES
+########
 import matplotlib.pyplot as plt
+import numpy as np
 
-def main():
-    pass
+data = np.array([[1000,1200,998,800,400]]).T
+for i in range(len(data)):
+    plt.plot(i,data[i],'rx',label="MEASURED VALUE")
 
-if __name__=="__main__":
-    main()
+H = np.array([[1,1,1,1,1]]).T
+print(np.matmul(H.T,H))
+k = np.matmul(H.T,H)
+p = np.linalg.inv(k)
+# print(np.matmul(np.array(1/(int(np.matmul(H.T,H)))),H.T))
+estimate = np.matmul(np.matmul(p,H.T),data)
+plt.plot(i,estimate,'cx',label="ESTIMATED VALUE")
+plt.legend()
+plt.show()
